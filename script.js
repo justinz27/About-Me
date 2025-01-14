72f1862711d001b0a4b7798f065acd862abcd82e
justinz27 <justinzaia@outlook.com>
2025-01-14 17:24:13 +1100

    Uploading website
    uploaded files to it

---
+++ script.js
@@ -0,0 +1,20 @@
+const fadeIns = document.querySelectorAll('.fade-in');
+
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('visible');
+                }
+            });
+        });
+fadeIns.forEach(element => observer.observe(element));
+
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
